5e708400a73f820379c46bb3dbfea17d42bdd277
brent-soles <b.d.soles@gmail.com>
2019-04-12 23:13:22 -0700

    revamp: carousel working

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -5,7 +5,6 @@ import Content from './Content';
 import GitHubHOC from './Projects';
 import styled from '@emotion/styled';
 import GithubHOC from './Projects';
-import { red } from 'ansi-colors';
 
 const Card = styled.div`
   border-radius: 12px;
@@ -29,8 +28,61 @@ const Card = styled.div`
   }
 `;
 
+const FullCard = ({ imgSrc, }) => {
+  
+  return (
+    <Card>
+
+    </Card>
+  )
+}
+
+const StackCarousel = ({ items }) => {
+  const [order, setOrder] = React.useState(items);
+  return (
+    <>
+      {order.map((el, i) => {
+        if (i > 0 ) {
+          return (
+            <Card
+              key={i}
+              style={{ ...el.style, ...el.positioning }}
+              onClick={() => {
+                let first = order[0];
+                let replace = order[i];
+                let arr = order;
+                // Switch styling/positoning
+                let tmpPos = first.positioning;
+                first.positioning = replace.positioning;
+                replace.positioning = tmpPos;
+                arr.splice(0, 1, replace);
+                arr.splice(i, 1, first);
+                setOrder([...arr]);
+              }}
+            >
+              <img src={el.imgSrc} height="100px"></img>
+            </Card>
+          )
+
+        }
+        //Return the center card
+        return (
+          <Card
+            key={i}
+            style={{ ...el.style, ...el.positioning }}
+          >
+            <img src={el.imgSrc} height="100px"></img>
+          </Card>
+        )
+      })}
+    </>
+  )
+}
+
 
 const MainBanner = ({ projects }) => {
+
+
   return (
     <>
       <img src="/static/main-header-2.svg" style={{width: '100%', position: 'relative', zIndex: 9}}></img>
@@ -44,93 +96,50 @@ const MainBanner = ({ projects }) => {
           // paddingLeft: '39%'
         }}
       >
-        <Card
-          style={{
-            zIndex: 8, 
-            backgroundColor: 'red',
-            position: 'absolute',
-            left: '50%',
-            transform: 'translate(-113%, 17%) scale(0.7);',
-            opacity: .27
-          }}
-        >
-          <img src="/static/svgs/github.svg" height="160px"
-            style={{
-              borderRadius: '160px'
-            }}
-          ></img>
-          <h1>LEFT</h1>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-        </Card>
-        <Card
-          style={{
-            zIndex: 10, 
-            backgroundColor: '#fff',
-            position: 'absolute',
-            left: '50%',
-            transform: 'translate(-50%, 0%)',
-            boxShadow: '0px 3px 8px 1px rgba(0, 0, 0, .3)'
-          }}
-        >
-          <img src="/static/images/brent-lq.jpg" height="160px"
-            style={{
-              borderRadius: '160px'
-            }}
-          ></img>
-          <h1>CENTER</h1>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-        </Card>
-        <Card
-          style={{
-            zIndex: 8, 
-            backgroundColor: 'green',
-            position: 'absolute',
-            transform: 'translate(13%, 17%) scale(0.7);',
-            opacity: .27,
-            left: '50%'
-          }}
-        >
-          <img src="/static/svgs/code.svg" height="160px"
-            style={{
-              borderRadius: '160px'
-            }}
-          ></img>
-          <h1>RIGHT</h1>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-          <p>
-            Content content content
-          </p>
-        </Card>
+        <StackCarousel items={[
+          {
+            imgSrc: "/static/images/brent-lq.jpg",
+            heading: 'Center',
+            style: {
+              backgroundColor: '#fff',
+            },
+            positioning: {
+              zIndex: 10,
+              position: 'absolute',
+              left: '50%',
+              transform: 'translate(-50%, 0%)',
+              boxShadow: '0px 3px 8px 1px rgba(0, 0, 0, .3)'
+            }
+          },
+          {
+            imgSrc: '/static/svgs/github.svg',
+            heading: 'Left',
+            style: {
+              backgroundColor: 'red',
+            },
+            positioning: {
+              zIndex: 8,
+              position: 'absolute',
+              left: '50%',
+              transform: 'translate(-113%, 17%) scale(0.7)',
+              opacity: .27
+            }
+          },
+          {
+            imgSrc: '/static/svgs/code.svg',
+            heading: 'Right',
+            style: {
+              backgroundColor: 'green'
+            },
+            positioning: {
+              zIndex: 8,
+              position: 'absolute',
+              transform: 'translate(13%, 17%) scale(0.7)',
+              opacity: .27,
+              left: '50%'
+            }
+          }
+        ]} />
       </div>
     </>
   )
