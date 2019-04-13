da6a57901389c870c1172926ca8d3026c5cb7a2f
brent-soles <b.d.soles@gmail.com>
2019-04-12 20:27:34 -0700

    revamp: rough outline

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -5,126 +5,134 @@ import Content from './Content';
 import GitHubHOC from './Projects';
 import styled from '@emotion/styled';
 import GithubHOC from './Projects';
+import { red } from 'ansi-colors';
 
+const Card = styled.div`
+  border-radius: 12px;
+  align-items: center;
+  background: yellow;
+  max-width: 50%;
+  min-width: 500px;
+  min-height: 500px;
+  margin: 0px auto;
+  margin-top: -6%;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  z-index: 10;
 
-const gradientProps = {
-  primary: '#BB6BD9',
-  secondary: '#A8F6D1'
-}
-
-const GradientDiv = styled.div`
-  position: relative;
-  width: 100%;
-  background: ${({ primary, secondary }) => `linear-gradient(137deg, ${primary} 32%, ${secondary} 100%)`};
-
-  * {
-    position: absolute;
+  transition: .25s ease-in-out;
+  &:hover {
+    box-shadow: 0px 3px 12px 1px rgba(0, 0, 0, .3);
   }
 `;
 
-const BannerH1 = styled.h1`
-  max-width: 40rem;
-  min-width: 30rem;
-  margin: 0px auto;
-  left: 50%;
-  transform: translateX(-50%);
-  color: rgba(255, 255, 255, .9);
-  font-family: POLYA;
-  font-style: normal;
-  font-weight: normal;
-  line-height: 14.5rem;
-  font-size: 8.65rem;
-  text-align: center;
-  letter-spacing: 0.075em;
-`;
-
-const BannerH2 = styled.h2`
-  width: 30rem;
-  min-width: 20rem;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  top: 50%;
-  color: rgba(0, 0, 0, .2);
-  font-family: POLYA;
-  font-style: normal;
-  font-weight: normal;
-  line-height: 7.25rem;
-  font-size: 7.25rem;
-  text-align: center;
-  letter-spacing: 0.075em;
-  filter: blur(2px);
-`;
-
-const H1 = styled.h1`
-    font-family: FARRAY;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 2.4rem;
-    line-height: 28px;
-    color: #878787;
-`
-const P = styled.p`
-    margin-top: 6rem;
-    font-size: 2rem;
-    font-family: Helvetica;
-    color: #878787;
-`;
 
 const MainBanner = ({ projects }) => {
   return (
-    <MainPage>
-      <GradientDiv {...gradientProps}>
-        <BannerH1>
-          BRENT SOLES
-              </BannerH1>
-        <BannerH2>
-          BRENT SOLES
-              </BannerH2>
-      </GradientDiv>
-      <Content
-        id={"about"}
-        imgSrc={'/static/images/brent-mq.jpg'}
-        alt={'muuuuuugshot'}
-        size={'14.4rem'}
-        pageName={`WHO I AM`}
+    <>
+      <img src="/static/main-header-2.svg" style={{width: '100%', position: 'relative', zIndex: 9}}></img>
+      <div
+        style={{
+          background: 'red',
+          width: '100%',
+          // display: 'flex',
+          // flexDirection: 'row',
+          position: 'relative',
+          // paddingLeft: '39%'
+        }}
       >
-        <H1>I'm a Christian, a developer and an aspiring designer</H1>
-        <P>
-          I am based out of Portland, OR; and have lived in the PNW for the past 3 years.
-          My first passion in life is my faith. I am a Christian, soon-to-be husband, and total nerd. 
-          I even have the glasses to prove it!
-        </P>
-        <P>
-          My expertise is in fullstack development, with an emphsis on React, Node.js and MongoDB.
-          In this site you'll find some of my personal projects, as well as some services I offer as a freelancer (soon hopefull a blog!).
-        </P>
-        <P>
-          If you'd like to reach out, I'd love to talk with you! There is a form at the bottom of the page.
-        </P>
-        <P>
-          God bless, and thank you for taking the time to read!
-        </P>
-        <P>
-          - Brent
-        </P>
-      </Content>
-      <Content
-        id={"projects"}
-        imgSrc={'/static/svgs/github.svg'}
-        alt={'prjs plz'}
-        size={'14.4rem'}
-        pageName={`PROJECTS`}
-      >
-        <GithubHOC projects={projects} />
-      </Content>
-      {/* <Content
-        id={"services"}
-        imgSrc={'/static/svgs/code.svg'}
-        alt={'hire me'}
-        size={'14.4rem'}
-        pageName={`SERVICES`}
-      ></Content> */}
-    </MainPage>
+        <Card
+          style={{
+            zIndex: 8, 
+            backgroundColor: 'red',
+            position: 'absolute',
+            left: '50%',
+            transform: 'translate(-113%, 17%) scale(0.7);',
+            opacity: .27
+          }}
+        >
+          <img src="/static/svgs/github.svg" height="160px"
+            style={{
+              borderRadius: '160px'
+            }}
+          ></img>
+          <h1>LEFT</h1>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+        </Card>
+        <Card
+          style={{
+            zIndex: 10, 
+            backgroundColor: '#fff',
+            position: 'absolute',
+            left: '50%',
+            transform: 'translate(-50%, 0%)',
+            boxShadow: '0px 3px 8px 1px rgba(0, 0, 0, .3)'
+          }}
+        >
+          <img src="/static/images/brent-lq.jpg" height="160px"
+            style={{
+              borderRadius: '160px'
+            }}
+          ></img>
+          <h1>CENTER</h1>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+        </Card>
+        <Card
+          style={{
+            zIndex: 8, 
+            backgroundColor: 'green',
+            position: 'absolute',
+            transform: 'translate(13%, 17%) scale(0.7);',
+            opacity: .27,
+            left: '50%'
+          }}
+        >
+          <img src="/static/svgs/code.svg" height="160px"
+            style={{
+              borderRadius: '160px'
+            }}
+          ></img>
+          <h1>RIGHT</h1>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+          <p>
+            Content content content
+          </p>
+        </Card>
+      </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,32 @@ import React from 'react';
 import MainBanner from '../components/MainBanner';
 
 const App = (props) => {
-    return (
-        <>
-            <MainBanner {...props} />
-        </> 
-    )
+  return (
+    <>
+      <MainBanner {...props} />
+    </>
+  )
 }
 
 App.getInitialProps = async () => {
-    try {
-        const res = await fetch(`http://localhost:8080/api/github`);
-        const projects = await res.json();
-        return {
-            projects
-        }
-    } catch (err) {
-        return {
-            projects: [
-                {
-                    name: `Sorry! My projects are unavailable`,
-                    html_url: `https://github.com/brent-soles`,
-                    description: `Please click the header to see my Github repo.`,
-                    language: null
-                }
-            ]
-        }
+  try {
+    const res = await fetch(`http://localhost:8080/api/github`);
+    const projects = await res.json();
+    return {
+      projects
     }
+  } catch (err) {
+    return {
+      projects: [
+        {
+          name: `Sorry! My projects are unavailable`,
+          html_url: `https://github.com/brent-soles`,
+          description: `Please click the header to see my Github repo.`,
+          language: null
+        }
+      ]
+    }
+  }
 }
 
 export default App;
